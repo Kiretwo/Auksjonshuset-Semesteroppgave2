567487e5efa4c553fa515d384aca3e807cabaccb
Erik <eritor00991@stud.noroff.no>
2024-12-16 23:34:32 +0100

    main.js cleanup

--- src/main.js
+++ src/main.js
@@ -42,28 +42,6 @@ async function init() {
         const profileModule = await import("./js/ui/profile/display.js");
         profileModule.initProfile();
         break;
-      /*
-      case "login":
-        const loginModule = await import("./js/ui/login.js");
-        loginModule.init();
-        break;
-      case "register":
-        const registerModule = await import("./js/ui/register.js");
-        registerModule.init();
-        break;
-      case "create-listing":
-        const createListingModule = await import("./ui/create-listing/create-listing.js");
-        createListingModule.init();
-        break;
-      case "edit-listing":
-        const editListingModule = await import("./ui/edit-listing/edit-listing.js");
-        editListingModule.init();
-        break;
-      case "delete-listing":
-        const deleteListingModule = await import("./ui/delete-listing/delete-listing.js");
-        deleteListingModule.init();
-        break;
-      */
       default:
         console.warn(`No module found for page: ${page}`);
     }
